d13cba658f1005ffcbdd4af2a38d788e8f72d691
Nite <nite.luo@samsung.com>
2019-03-01 14:59:10 -0800

    Adding Threading and Logging

--- thread/hello.cpp
+++ thread/hello.cpp
@@ -1,46 +1,109 @@
-#include <iostream>
 #include <thread>
+#include <chrono>
 #include <mutex>
 #include <vector>
 #include <time.h>
+#include <atomic>
+#include <condition_variable>
+
+#define LOGURU_WITH_STREAMS 1
+
+#include "../common/loguru.cpp"
 
 using namespace std;
 
 static const int num_threads = 8;
-static recursive_mutex mut;
+static const int buffer_count = 2;
+static const int cmd_count = 1000;
+static const int INVALID = INT_MAX;
+static mutex acquire_lock;
 
-static vector<int> buffer[2];
-static int currIndex = 0;
+struct CmdQueue
+{
+    vector<int> buffer;
+    atomic<bool> isReady;
+};
+
+static CmdQueue queues[buffer_count];
+
+int acquireBuffer(bool isReady){
+    lock_guard<mutex> guard(acquire_lock);
+
+    for(int i = 0; i < buffer_count; i++)
+    {
+        if(queues[i].isReady == isReady){
+            return i;
+        }
+    }
+    
+    return INVALID;
+}
 
 void doProduce(){
     int v;
-    v = rand() % 10;
+    int idx = acquireBuffer(false);
+
+    if(idx == INVALID){
+        LOG_S(INFO) << "All Queues are full";
+        this_thread::sleep_for(chrono::milliseconds(1));
+        return;
+    }
+
+    LOG_S(INFO) << "Filling buffer: " << idx;
+
+    for(size_t i = 0; i < cmd_count; i++)
+    {
+        v = rand() % 100;
+        queues[idx].buffer.push_back(v);
+    }
+    queues[idx].isReady = true;
+}
+
+void doConsume(){
+    int sum = 0;
+    int idx = acquireBuffer(true);
+    
+    if(idx == INVALID){
+        LOG_S(INFO) << "All buffers are empty";
+        this_thread::sleep_for(chrono::milliseconds(1));
+        return;
+    }
+
+    LOG_S(INFO) << "Consume buffer: " << idx;
+
+    for(auto it : queues[idx].buffer){
+        sum += it;
+    }
+    queues[idx].buffer.clear();
+    queues[idx].isReady = false;
+
+    LOG_S(INFO) << "Sum: " << sum;
+}
+
+void t_prod(int count){
+    for(size_t i = 0; i < count; i++)
+    {
+        doProduce();
+    }
+}
+
+void t_consume(int count){
+    for(size_t i = 0; i < count; i++)
+    {
+        doConsume();
+    }
     
 }
 
-void doConsume(int id){
-    lock_guard<recursive_mutex> lock(mut);
-
-    cout << "Hello from thread:" << id  << endl;
-}
-
-int main()
+int main(int argc, char* argv[])
 {
+    loguru::init(argc, argv);
     srand (time(NULL));
-    thread t[num_threads];
+    thread t1(t_prod, 10);
+    thread t2(t_consume, 10);
     
-    for(int i = 0; i < num_threads; i++)
-    {
-        t[i] = thread(doConsume, i);
-    }
-
-    //cout << "Launched from main" << endl;
-
-    for(int i = 0; i < num_threads; i++)
-    {
-        t[i].join();
-    }
-    
+    t1.join();
+    t2.join();
 
     return 0;
 }
