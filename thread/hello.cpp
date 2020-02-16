4d1b01d46ac2a5b9e1775d3cdf88c4c3c294e582
nite <darkdukey@gmail.com>
2020-02-15 21:50:55 -0800

    Log causing delay

--- thread/hello.cpp
+++ thread/hello.cpp
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <atomic>
 #include <chrono>
+#include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
@@ -18,6 +19,7 @@ static const int cmd_count = 1000;
 static const int FPS_TIME = 16666;
 static const int INVALID = INT_MAX;
 static mutex acquire_lock;
+static bool stop = false;
 
 // simulate command queue
 struct CmdQueue {
@@ -45,73 +47,70 @@ void doProduce() {
     int idx = acquireBuffer(false);
     steady_clock::time_point time_start, time_end;
 
-    time_start = steady_clock::now();
-
     if (idx == INVALID) {
-        LOG_S(INFO) << "All Queues are full";
+        LOG_S(WARNING) << "All Queues are full: Sleep";
         this_thread::sleep_for(milliseconds(1));
         return;
     }
 
-    LOG_S(INFO) << "Filling buffer: " << idx;
+    LOG_S(INFO) << "* Filling buffer: " << idx;
 
+    time_start = steady_clock::now();
     for (size_t i = 0; i < cmd_count; i++) {
         v = rand() % 100;
         queues[idx].buffer.push_back(v);
     }
     queues[idx].isReady = true;
-
     time_end = steady_clock::now();
 
     int t, remaining_time;
     t = duration_cast<microseconds>(time_end - time_start).count();
+
+    LOG_S(INFO) << "* Product Time: " << t;
     remaining_time = FPS_TIME - t;
     if (remaining_time > 0) {
         this_thread::sleep_for(microseconds(remaining_time));
     }
-
-    LOG_S(INFO) << "Time: " << t;
 }
 
 void doConsume() {
     int sum = 0;
     int idx = acquireBuffer(true);
 
-    auto time_start = steady_clock::now();
-
     if (idx == INVALID) {
-        LOG_S(INFO) << "All buffers are empty";
+        LOG_S(WARNING) << "All buffers are empty: Sleep";
         this_thread::sleep_for(milliseconds(1));
         return;
     }
 
-    LOG_S(INFO) << "Consume buffer: " << idx;
+    LOG_S(INFO) << "= Consume buffer: " << idx;
 
+    auto time_start = steady_clock::now();
     for (auto it : queues[idx].buffer) {
         sum += it;
     }
     queues[idx].buffer.clear();
     queues[idx].isReady = false;
-
-    LOG_S(INFO) << "Sum: " << sum;
-
     auto time_end = steady_clock::now();
 
     int t = duration_cast<microseconds>(time_end - time_start).count();
+    LOG_S(INFO) << "= Sum: " << sum;
+    LOG_S(INFO) << "= Consume Time: " << t;
+
     int remaining_time = FPS_TIME - t;
     if (remaining_time > 0) {
         this_thread::sleep_for(microseconds(remaining_time));
     }
 }
 
-void t_prod(int count) {
-    for (size_t i = 0; i < count; i++) {
+void t_prod() {
+    while (!stop) {
         doProduce();
     }
 }
 
-void t_consume(int count) {
-    for (size_t i = 0; i < count; i++) {
+void t_consume() {
+    while (!stop) {
         doConsume();
     }
 }
@@ -119,8 +118,11 @@ void t_consume(int count) {
 int main(int argc, char* argv[]) {
     loguru::init(argc, argv);
     srand(time(NULL));
-    thread t1(t_prod, 10);
-    thread t2(t_consume, 20);
+    thread t1(t_prod);
+    thread t2(t_consume);
+    char ch_quit;
+    cin >> ch_quit;
+    stop = true;
 
     t1.join();
     t2.join();
