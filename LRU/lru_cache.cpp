62471258dad0e292d59608e2345e86be0bfa1c75
nite <darkdukey@gmail.com>
2020-03-21 00:18:49 -0700

    implement LRU

--- LRU/lru_cache.cpp
+++ LRU/lru_cache.cpp
@@ -3,10 +3,20 @@
 #include <unordered_map>
 using namespace std;
 
+class Node {
+   public:
+    int key;
+    int value;
+
+   public:
+    Node(int k, int v) : key(k), value(v){};
+    ~Node(){};
+};
+
 class LRUCache {
    private:
-    list<int> _dq;
-    unordered_map<int, list<int>::iterator> _mp;
+    list<Node> _dq;
+    unordered_map<int, list<Node>::iterator> _mp;
     size_t _size;
 
    public:
@@ -18,9 +28,9 @@ class LRUCache {
         if (_mp.find(key) == _mp.end()) {
             val = -1;
         } else {
-            val = *_mp[key];
+            val = (*_mp[key]).value;
             _dq.erase(_mp[key]);
-            _dq.push_front(val);
+            _dq.emplace_front(key, val);
             _mp[key] = _dq.begin();
         }
         return val;
@@ -31,35 +41,43 @@ class LRUCache {
         } else {
             //Pop the last element
             if (_dq.size() == _size) {
-                int last = _dq.back();
+                Node last = _dq.back();
                 _dq.pop_back();
-                _mp.erase(last);
+                _mp.erase(last.key);
             }
         }
         // Put the recent visited element to the front
-        _dq.push_front(value);
+        _dq.emplace_front(key, value);
         _mp[key] = _dq.begin();
     }
     void print() {
         cout << "[";
-        for (auto it = _dq.begin(); it != _dq.end(); it++)
-            cout << (*it) << " ";
+        for (auto& it : _dq)
+            cout << (it.value) << " ";
         cout << "]" << endl;
     }
 };
 
-int main(int argc, char const *argv[]) {
-    LRUCache cache(4);
-    cache.put(1, 1);
-    cache.put(2, 2);
-    cache.put(5, 5);
-    cache.put(6, 6);
-    cache.get(1);     // returns 1
-    cache.get(2);     // returns -1 (not found)
-    cache.put(3, 3);  // evicts key 2
-    cache.put(4, 4);  // evicts key 1
-    cache.get(1);     // returns -1 (not found)
-    cache.get(3);     // returns 3
-    cache.get(4);     // returns 4
+int main(int argc, char const* argv[]) {
+    // LRUCache cache(4);
+    // cache.put(1, 1);
+    // cache.put(2, 2);
+    // cache.put(5, 5);
+    // cache.put(6, 6);
+    // cache.get(1);     // returns 1
+    // cache.get(2);     // returns -1 (not found)
+    // cache.put(3, 3);  // evicts key 2
+    // cache.put(4, 4);  // evicts key 1
+    // cache.get(1);     // returns -1 (not found)
+    // cache.get(3);     // returns 3
+    // cache.get(4);     // returns 4
+    // cache.print();
+
+    LRUCache cache(1);
+    cache.put(2, 1);
+    cache.get(2);
+    cache.put(3, 2);
+    cache.get(2);
+    cache.get(3);
     cache.print();
 }
